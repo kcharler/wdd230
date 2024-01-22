752c8239a997465043a5b48f3d88b45918b4e7b5
kcharler <kou17002@byui.edu>
2024-01-22 14:14:39 -0700

    Update

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
+const links = body.querySelectorAll('main div li');
 
 const hamburgerToggle = document.querySelector("#hamburger-toggle");
 const navElement = document.querySelector(".navigation");
@@ -9,23 +10,28 @@ hamburgerToggle.addEventListener("click", () => {
     hamburgerToggle.classList.toggle('open');
 });
 
-toggle.addEventListener('click', function(){
+toggle.addEventListener('click', function () {
     this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
+    if (this.classList.toggle('bi-brightness-high-fill')) {
         body.style.background = 'white';
         body.style.color = 'black';
         body.style.transition = '2s';
         body.querySelector('main h1').style.color = 'black';
         body.querySelector('main section h3').style.color = 'black';
+        links.forEach(link => {
+            link.style.color = 'black';
+        });
         body.querySelector('main h1').style.borderBottom = '3px solid black';
-    }else{
+    } else {
         body.style.background = 'black';
         body.style.color = 'white';
         body.style.transition = '2s';
         body.querySelector('main h1').style.color = 'white';
         body.querySelector('main section h3').style.color = 'white';
+        links.forEach(link => {
+            link.style.color = 'white';
+        });
         body.querySelector('main h1').style.borderBottom = '3px solid white';
 
     }
 });
-
