57ac1046e43cdfcafca978308f5e02f2c9e0c16c
kcharler <kou17002@byui.edu>
2024-01-22 09:16:22 -0700

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,14 @@
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 
+const hamburgerToggle = document.querySelector("#hamburger-toggle");
+const navElement = document.querySelector(".navigation");
+
+hamburgerToggle.addEventListener("click", () => {
+    navElement.classList.toggle('open');
+    hamburgerToggle.classList.toggle('open');
+});
+
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-moon');
     if(this.classList.toggle('bi-brightness-high-fill')){
@@ -11,5 +19,9 @@ toggle.addEventListener('click', function(){
         body.style.background = 'black';
         body.style.color = 'white';
         body.style.transition = '2s';
+        body.querySelector('main h2').style.color = 'white';
+        body.querySelector('main h2').style.borderBottom = '3px solid white';
+
     }
 });
+
